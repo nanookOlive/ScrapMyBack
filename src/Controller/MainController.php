1f0b5d814b5281d5863f7d18a732fbfa669b28e8
nanookOlive <olivier.stierer@gmail.com>
2023-11-14 12:48:29 +0100

    cleaning

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,7 +15,7 @@ class MainController extends AbstractController
 {
     //méthode qui affiche l'ensemble des jeux temporaires disponibles désormais en base 
     #[Route('/', name: 'app_main')]
-    public function index(GameTmpRepository $gameRepo): Response
+    public function index(GameRepository $gameRepo): Response
     {
     
         $arrayGameTmp=$gameRepo->findAll();
@@ -23,37 +23,9 @@ class MainController extends AbstractController
        
     }
 
-    //méthode qui va afficher le détai d'un jeux en scraping
-    #[Route("/scrapyDo",name: "app_scrapyDo")]
-    public function show(Scrapy $scrapy,GameTmpRepository $gameRepo){
 
-
-        set_time_limit(0);//le temps de passé à rercher peut être long
-        //on veut éviter de le processus s'arreter avant la fin de la recherche
-        $arrayGameTmp=$gameRepo->findAll();
-
-        foreach($arrayGameTmp as $gameTmp){
-
-                $game=$scrapy->crawlerDetail($gameTmp);
-
-        }
-       
-        return $this->render("main/resultat.html.twig");
-    }
-
-    //route qui va créer un ensemble de jeux temporaires en base de données
-    //ils n'ont que deux champs leur nom et l'url qui amène sur la page de détail d'un sur le site play in
-    //on peut décider du nombre de pages qui seront scrapper
-    #[Route('/create',name:'app_init_db')]
-    public function create(Scrapy $scrapy){
-        
-        $scrapy->getListGames(10);
-        return $this->redirectToRoute('app_main');
-
-    }
-    #[Route("/base/show/{id}",name:'app_base_show')]
+    #[Route("/show/{id}",name:'app_base_show')]
     public function showBase(Game $game,GameRepository $gameRepo){
-        //$gameFound=$gameRepo->find($game->getId());
 
         $gameFound=$gameRepo->findGameAllData($game);
 
@@ -61,13 +33,5 @@ class MainController extends AbstractController
 
     }
 
-    // #[Route("/show/{id}",name:'app_base_show')]
-    // public function showGame(GameTmp $gameTmp,GameRepository $gameRepo){
-    //     //$gameFound=$gameRepo->find($game->getId());
-
-    //     $gameFound=$gameRepo->findGameAllData($game);
-
-    //     return $this->render("main/show.html.twig",['data'=>$gameFound]);
-
-    // }
+  
 }
